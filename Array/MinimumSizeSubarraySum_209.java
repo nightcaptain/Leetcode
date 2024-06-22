5d9a60971b0011be229cd8598f5cf573af7dbeeb
ZhenQing zhao <131536576+nightcaptain@users.noreply.github.com>
2024-06-22 15:32:29 +0800

    6.22

--- Array/MinimumSizeSubarraySum_209.java
+++ Array/MinimumSizeSubarraySum_209.java
@@ -1,6 +1,8 @@
 package Leetcode.Leetcode.Array;
 
 public class MinimumSizeSubarraySum_209 {
+
+    /*暴力解法
     public static int minSubArrayLen(int target, int[] nums) {
 
         int result = Integer.MAX_VALUE;
@@ -20,7 +22,24 @@ public class MinimumSizeSubarraySum_209 {
         }
         return result == Integer.MAX_VALUE ? 0 : result;
     }
+    */
+    public static int minSubArrayLen(int target, int[] nums) {
+        int result = Integer.MAX_VALUE;
+        int sum = 0;//滑动窗口内元素之和
+        int left = 0;//滑动窗口起始位置
+        int subLength = 0;//滑动窗口的长度
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
 
+            while (sum >= target) {
+                subLength = right - left + 1;
+                result = result < subLength ? result : subLength;
+                sum -= nums[left++];
+            }
+        }
+
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
     public static void main(String[] args) {
         int[] nums = new int[]{2,3,1,2,4,3};
         int target = 7;
